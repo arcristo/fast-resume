0503c56ddfa85cc7e0f5ffcdb0d22de8fb7b1ede
Adiel Cristo <adielcristo@gmail.com>
2017-07-14 15:18:42 -0300

    #8: Add profile form and initial layout

--- app/config/prod.php
+++ app/config/prod.php
@@ -1,6 +1,16 @@
 <?php
 
-// configure your app for the production environment
+use Silex\Provider\FormServiceProvider;
 
+// Configure app for the production environment
 $app['twig.path'] = array(APP_PATH . '/templates');
 $app['twig.options'] = array('cache' => ROOT_PATH . '/var/cache/twig');
+
+// Register locale provider
+$app->register(new Silex\Provider\LocaleServiceProvider());
+
+// Register and configure form provider
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
--- app/controllers/DefaultController.php
+++ app/controllers/DefaultController.php
@@ -1,8 +1,34 @@
 <?php
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('default/index.html.twig', array());
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+$app->match('/', function (Request $request) use ($app) {
+    // default form data
+    $data = array(
+        'user' => null,
+    );
+
+    $form = $app['form.factory']->createBuilder(FormType::class, $data)
+        ->add('user')
+        ->add('submit', SubmitType::class, [
+            'label' => 'Open',
+        ])
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+
+        return $app->redirect('https://www.linkedin.com/in/' . $data['user']);
+    }
+
+    // display the form
+    return $app['twig']->render('default/index.html.twig', array('form' => $form->createView()));
 })
-->bind('homepage');
+    ->method('GET|POST')
+    ->bind('homepage');
 
 return $app;
