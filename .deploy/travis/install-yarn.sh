d3574edb81f171729698ede2d83a627e501b6bbb
Adiel Cristo <adielcristo@gmail.com>
2017-07-24 03:30:38 -0300

    #17: Enabling exit on script error

--- .deploy/travis/install-yarn.sh
+++ .deploy/travis/install-yarn.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Exit on error
+set -e
+set -o pipefail
+
 # Install node
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sSL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Install yarn
