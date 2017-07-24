1f1d5401ef051aeaaa12e0bd63feac06a2a8f933
Adiel Cristo <adielcristo@gmail.com>
2017-07-24 03:09:31 -0300

    #17: Update distro version to precise

--- .deploy/travis/install-yarn.sh
+++ .deploy/travis/install-yarn.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# Install node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 # Install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
-echo "export PATH=\"$PATH:$HOME/.yarn/bin\"" >> ~/.profile
 
 # Check version
-~/.yarn/bin/yarn --version
+yarn --version
