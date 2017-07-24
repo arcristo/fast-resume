1545cd8ed785d437ab7f0e53bc392d4e12dc41b4
Adiel Cristo <adielcristo@gmail.com>
2017-07-24 01:33:38 -0300

    #17: Adding yarn to $PATH

--- .deploy/travis/install-yarn.sh
+++ .deploy/travis/install-yarn.sh
@@ -4,6 +4,7 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
+echo "export PATH=\"$PATH:$HOME/.yarn/bin\"" >> ~/.profile
 
 # Check version
-yarn --version
+~/.yarn/bin/yarn --version
